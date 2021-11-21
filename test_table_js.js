f15db826989d47d3e09d6cef9d6fe63e03201099
Yofti <yofti.milkessa@yale.edu>
2021-11-21 04:12:25 -0500

    A handful of useful changes.

--- test_table_js.js
+++ test_table_js.js
@@ -1,18 +1,15 @@
-let currentSelection = "11";
+let currentSelection = "a11";
 
 function startCellSelection() {
     var element = document.getElementById(currentSelection);
     element.classList.add("selected");
 }
 
-window.onload = startCellSelection();
-
 // Do this when a cell gets clicked on
-function clickResponse(some_id){
+function clickResponse(some_id) {
     var clickedID = some_id;
-    console.log(clickedID);
     if (clickedID == currentSelection) {
-        continue;
+        return;
     }
     else if (clickedID != "") {
         // First fetch both the original cell and the one clicked on
@@ -20,7 +17,38 @@ function clickResponse(some_id){
         var new_item = document.getElementById(clickedID);
 
         // Unselect the old one; select the new one
-        old_item.classList.remove(selected);
-        new_item.classList.add(selected);
+        old_item.classList.remove("selected");
+
+        currentSelection = clickedID;
+        new_item.classList.add("selected");
     }
-}
+};
+
+document.onkeydown = function(e) {
+    curr_id = currentSelection.split("");
+    row = parseInt(curr_id[1]);
+    col = parseInt(curr_id[2]);
+
+    switch (e.keyCode) {
+        case 37:
+            new_col = ((col + 3) % 5) + 1;
+            new_id = curr_id[0] + row + new_col
+            break;
+        case 38:
+            new_row = ((row + 3) % 5) + 1;
+            new_id = curr_id[0] + new_row + col;
+            break;
+        case 39:
+            new_col = ((col - 1) % 5) + 1;
+            new_id = curr_id[0] + row + new_col
+            break;
+        case 40:
+            new_row = ((row - 1) % 5) + 1;
+            new_id = curr_id[0] + new_row + col;
+            break;
+    }
+
+    clickResponse(new_id);
+};
+
+window.onload = startCellSelection();
